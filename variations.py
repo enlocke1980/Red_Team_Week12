e1075ae4481362ea95f602903411444f399af192
John Doe <johndoe@email.com>
2023-02-07 00:58:47 +0000

    Variations

---
+++ variations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.7
+
+# Python implementation here
+
+message = input("Enter a message: ")
+
+# Use the phrase "This is a test message"
+
+print("Lowercase:", message.lower())
+print("Uppercase:", message.upper())
+print("Capitalized:", message.capitalize())
+print("Title Case:", message.title())
+
+words = message.split()
+print("Words:", words)
+
+sorted_words = sorted(words)
+print("Alphabetic First Word:", sorted_words[0])
+print("Alphabetic Last Word:", sorted_words[-1])
